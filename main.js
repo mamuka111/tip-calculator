1bd2e6266ff98cb089a97719b454761b3f078afe
mamuka111 <123620275+mamuka111@users.noreply.github.com>
2023-03-05 22:12:34 +0400

    Javascript paart1

---
+++ main.js
@@ -0,0 +1,6 @@
+let input = document.querySelector(".input");
+let costumInput = document.querySelector(".costumInput");
+let personInput = document.querySelector(".personInput");
+let p3 = document.querySelector(".p3")
+let p4 = document.querySelector(".p4")
+
